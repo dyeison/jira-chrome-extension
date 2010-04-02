e7fafd734e3d709ecd88697f443a0084b83fa92a
andry.virvich@gmail.com <andry.virvich@gmail.com@bcdd7733-2674-7b9f-073b-6a372a3830da>
2010-04-02 07:25:29 +0000

    automation for releases

---
+++ trunk/js/loader.js
@@ -0,0 +1,83 @@
+var loader = {
+	resolutions: [],
+	filters: [],
+	token: null,
+	url:null,
+	login: function(username, password, callback){
+			var pl = new SOAPClientParameters();
+			pl.add("username", username);
+			pl.add("password", password);
+				SOAPClient.invoke(loader.url + "/rpc/soap/jirasoapservice-v2", "login", pl, true, function(r, xhr){
+					if($("faultstring",xhr).text() != '')
+					{
+						localStorage.setItem("error", $("faultstring",xhr).text());
+						chrome.browserAction.setIcon({ 'path' : '16x16off.png'});
+						chrome.browserAction.setBadgeText({text: ''});
+					} else {
+						localStorage.setItem("error", "");
+						loader.token = $(xhr).text();
+						loader.getSettings();
+						callback(loader);
+					}
+				});
+	},
+	update: function(){
+	
+				var pl = new SOAPClientParameters();
+				pl.add("in0", loader.token);
+				pl.add("in1", "assignee = currentUser() AND resolution is EMPTY ORDER BY priority DESC, created ASC");
+				//pl.add("in1", "resolution is EMPTY ORDER BY priority DESC, created ASC");
+				pl.add("in2",100);
+				SOAPClient.invoke(loader.url + "/rpc/soap/jirasoapservice-v2", "getIssuesFromJqlSearch", pl, true, function(r, xhr){
+						
+						chrome.browserAction.setIcon({ 'path' : '16x16.png'});
+						chrome.browserAction.setBadgeText({text: $("assignee", xhr).size().toString()});
+						localStorage.setItem("AssignedToMe", $(xhr).xml(1));
+				});
+				loader.getSavedFilters();
+	},
+	getSettings: function(){
+				var pl = new SOAPClientParameters();
+				pl.add("in0", loader.token);
+				SOAPClient.invoke(loader.url + "/rpc/soap/jirasoapservice-v2", "getResolutions", pl, true, function(r, xhr){
+					$(xhr).find("multiRef").each(function(i, val) {
+						if($("id", val).text()){
+							loader.resolutions[$("id", val).text()] = $("name", val).text();
+						}
+					});
+				});
+	},
+	getSavedFilters: function(){
+				var pl = new SOAPClientParameters();
+				pl.add("in0", loader.token);
+				
+				SOAPClient.invoke(loader.url + "/rpc/soap/jirasoapservice-v2", "getSavedFilters", pl, true, function(r, xhr){
+					loader.filters.length = 0;
+					$(xhr).find("multiRef").each(function(i, val) {
+						if($("id", val).text()){
+							
+							loader.filters.push({
+								id: $("id", val).text(),
+								name: $("name", val).text()
+							});
+							loader.getIssuesFromFilter($("id", val).text());
+						}
+					});
+				});
+	},
+	getIssuesFromFilter: function(filterid){
+				var pl = new SOAPClientParameters();
+				pl.add("in0", loader.token);
+				pl.add("in1", filterid);
+				//pl.add("in1", "resolution is EMPTY ORDER BY priority DESC, created ASC");
+				SOAPClient.invoke(loader.url + "/rpc/soap/jirasoapservice-v2", "getIssuesFromFilter", pl, true, function(r, xhr){
+						//alert($(xhr).xml());
+						localStorage.setItem("IssuesFromFilter_" + filterid , $(xhr).xml(1));
+				});
+	},
+	updateIssuesFromFilter: function(){
+		$.map(loader.filters, function(){
+		
+		});
+	}
+}
